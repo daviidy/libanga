4cb53c50423f7fa8f8da3252fb356601d012a394
aouman <k2.aouman@gmail.com>
2020-11-20 18:06:19 +0000

    La section nos artistes. Cette section est divisée en col-md-2. Elle contient une image, un titre et un paragraphe. J'ai mis un shadow pour les fait ressortir afin qu'elle ne soit pas confondue

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,7 +30,7 @@
               </li>
             </ul>
             <div class="mt-md-0 mt-4">
-              <a href="#" class="text-decoration-none h-auto rounded-pill py-3 px-5 text-white btn-h-2 btn-shadow">
+              <a href="#" class="text-decoration-none h-auto rounded-pill py-3 px-5 text-white btn-h-2 btn-shadow" data-toggle="modal" data-target="#exampleModal">
                 S'inscrire
               </a>
             </div>
@@ -53,7 +53,7 @@
             <div class="col-12 col-md-7 d-flex flex-column cta px-md-3 px-0 py-5">
               <p class=" mb-md-5 font-weight-bold display-4"> <span class="font-weight-lighter">Bienvenu sur  </span>Libanga</p>
               <div class="text-left mt-md-5 my-4">
-                <a href="#" class="text-decoration-none box-hover h-auto rounded-pill py-3 px-5 mt-3 mb-5 text-white btn-h btn-shadow">S'inscrire</a>
+                <a href="#" class="text-decoration-none box-hover h-auto rounded-pill py-3 px-5 mt-3 mb-5 text-white btn-h btn-shadow" data-toggle="modal" data-target="#exampleModal">S'inscrire</a>
               </div>
             </div>
             <div class="col-12 col-md-5 image">
@@ -62,11 +62,123 @@
           </div>
         </div>
         <div class="wave d-md-block d-none">
-          <img src="/assets/images/wave.svg" alt="">
+          <img src="/assets/images/wave.png" alt="">
           <div class="spacer"></div>
         </div>
       </section>
+
+      <section class="container-fluid">
+        <div class="p-5">
+          <h2 class="h1 text-white font-weight-bold text-center text-uppercase">Nos artistes</h2>
+        </div>
+        <div class="row">
+          <div class="col-md-2">
+            <div class="p-3 box-shadow ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="p-3 box-shadow ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 ">
+            <div class="p-3 box-shadow">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </div>
+
+    <footer class="container-fluid bg-main">
+      <div class="p-md-4 p-3 text-white">
+          © <span id="year">2020 </span> LIBANGA. TOUS DROITS RÉSERVÉS.
+      </div>
+    </footer>
+
+        <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+      Launch demo modal
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
   </body>
