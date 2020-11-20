124f14a33ff3a90550e4d02414432827ce016e23
aouman <k2.aouman@gmail.com>
2020-11-20 21:52:36 +0000

    mise en place du popup d'inscription.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   </head>
-  <body>
+  <body class="bg-main">
     <div class="main-content bg-main">
       <header class="bg-hero">
         <nav class="navbar navbar-expand-lg navbar-light">
@@ -67,13 +67,13 @@
         </div>
       </section>
 
-      <section class="container-fluid">
+      <section class="container-fluid mb-md-5">
         <div class="p-5">
           <h2 class="h1 text-white font-weight-bold text-center text-uppercase">Nos artistes</h2>
         </div>
-        <div class="row">
+        <div class="row py-4">
           <div class="col-md-2">
-            <div class="p-3 box-shadow ">
+            <div class="p-3 box-shadow rounded ">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -84,7 +84,7 @@
             </div>
           </div>
           <div class="col-md-2">
-            <div class="p-3 box-shadow ">
+            <div class="p-3 box-shadow rounded ">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -96,7 +96,7 @@
           </div>
 
           <div class="col-md-2 ">
-            <div class="p-3 box-shadow">
+            <div class="p-3 box-shadow rounded">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -108,7 +108,7 @@
           </div>
 
           <div class="col-md-2">
-            <div class="p-3 box-shadow ">
+            <div class="p-3 box-shadow rounded ">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -120,7 +120,7 @@
           </div>
 
           <div class="col-md-2">
-            <div class="p-3 box-shadow ">
+            <div class="p-3 box-shadow rounded ">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -132,7 +132,80 @@
           </div>
 
           <div class="col-md-2">
-            <div class="p-3 box-shadow ">
+            <div class="p-3 box-shadow rounded ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div class="row py-4">
+          <div class="col-md-2">
+            <div class="p-3 box-shadow rounded ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <div class="p-3 box-shadow rounded ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2 ">
+            <div class="p-3 box-shadow rounded">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow rounded ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow rounded ">
+              <div class="">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
+              </div>
+              <div class="pt-2">
+                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
+                <p class="text-white">Nouvel album</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-md-2">
+            <div class="p-3 box-shadow rounded ">
               <div class="">
                 <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
               </div>
@@ -147,33 +220,48 @@
       </section>
     </div>
 
-    <footer class="container-fluid bg-main">
+    <footer class="container-fluid bg-main border-top-footer">
       <div class="p-md-4 p-3 text-white">
           © <span id="year">2020 </span> LIBANGA. TOUS DROITS RÉSERVÉS.
       </div>
     </footer>
 
-        <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-      Launch demo modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <!-- Modal-->
+    <div  class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+          <div class="bg-white rounded-lg">
+            <div class="position-relative text-center p-3" style="bottom:30px">
+              <div class="mx-auto">
+                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid rounded-circle avatar-login-img">
+              </div>
+              <span class="float-right position-relative rounded-circle svg-delete close" data-dismiss="modal" aria-label="Close"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                  <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg></span>
+            </div>
+            <form class="p-md-4 p-3 bg-white">
+              <p class="text-center h4 text-uppercase pb-3 font-weight-bold">Inscrire toi</p>
+              <div class="form-group input-group-lg">
+                <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="emailHelp" placeholder="Nom">
+              </div>
+              <div class="form-group input-group-lg">
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
+              </div>
+              <div class="form-group input-group-lg">
+                <input type="password" class="form-control" id="exampleInputPassword" placeholder="mot de pass">
+              </div>
+              <div class="form-group input-group-lg">
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirmer le mot de pass">
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+              </div>
+              <p>Avez-vous un compte? <a href="#">Connectez-vous</a> </p>
+              <div class="">
+                <button type="submit" class="btn btn-primary">S'inscrire</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
