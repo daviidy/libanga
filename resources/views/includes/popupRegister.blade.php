bd51ecc899c39adf63cfa2cb8d4ecd3f6f00a9f4
aouman <k2.aouman@gmail.com>
2020-11-21 11:05:05 +0000

    popup designe du modèle fournit

--- resources/views/includes/popupRegister.blade.php
+++ resources/views/includes/popupRegister.blade.php
@@ -1,39 +1,61 @@
 
 <!-- Modal-->
 <div  class="modal fade" id="modalRegister" tabindex="-1" aria-labelledby="modalRegisterLabel" aria-hidden="true">
+  <span class="float-right position-absolute rounded-circle svg-delete close" data-dismiss="modal" aria-label="Close"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+      <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+    </svg></span>
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content bg-transparent border-0">
       <div class="bg-transparent rounded-lg">
-        <div class="bg-success" style="border-radius:15px 15px  0 0;">
+        <div style="border-radius: 7px 7px 0 0; background:#e7cbc2">
           <div class="position-relative text-center p-3" style="bottom:30px">
             <div class="mx-auto">
               <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid rounded-circle avatar-login-img">
             </div>
-            <span class="float-right position-relative rounded-circle svg-delete close" data-dismiss="modal" aria-label="Close"><svg width="1.5em" height="1.5em" viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-              </svg></span>
+            <div class="">
+              <p class="w-75 mx-auto mb-0 text-center h4 pt-3">Connectez-vous</p>
+            </div>
           </div>
         </div>
-        <form class="p-md-4 p-3 bg-white" style="border-radius: 0 0 15px 15px;">
-          <div class="bg-white pt-md-2">
-            <p class="w-75 mx-auto bg-white mb-0 text-center h5 text-uppercase py-3 font-weight-bold">Connectez-vous</p>
-          </div>          <div class="form-group input-group-lg">
-            <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="emailHelp" placeholder="Nom">
+        <form >
+          <div class="p-md-4 p-2 bg-white">
+            <div class="input-group input-group-lg mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-user"></i></span>
+              </div>
+              <input type="text" class="form-control" id="exampleInputtext1" aria-describedby="emailHelp" placeholder="Nom">
+            </div>
+
+            <div class="input-group input-group-lg mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="far fa-envelope"></i></span>
+              </div>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
+            </div>
+
+            <div class="input-group input-group-lg mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-lock"></i></span>
+              </div>
+              <input type="password" class="form-control" id="exampleInputPassword" placeholder="mot de pass">
+            </div>
+
+            <div class="input-group input-group-lg mb-3">
+              <div class="input-group-prepend">
+                <span class="input-group-text" id="inputGroup-sizing-lg"><i class="fas fa-lock"></i></span>
+              </div>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirmer le mot de pass">
+            </div>
+
+            <p class="text-center">Avez-vous un compte? <a href="#" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target="#modalLogin" >Connectez-vous</a> </p>
           </div>
-          <div class="form-group input-group-lg">
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail">
-          </div>
-          <div class="form-group input-group-lg">
-            <input type="password" class="form-control" id="exampleInputPassword" placeholder="mot de pass">
-          </div>
-          <div class="form-group input-group-lg">
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Confirmer le mot de pass">
-          </div>
-          <p>Avez-vous un compte? <a href="#" data-dismiss="modal" aria-label="Close" data-toggle="modal" data-target="#modalLogin" >Connectez-vous</a> </p>
-          <div class="bg-primary p-md-3 text-center">
-            <p class="text-white mb-0">S'inscrire <span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
-</svg></span> </p>
+
+          <div class="p-md-4 p-3 text-center" type="submit" style="border-radius: 0 0 7px 7px; background:#6f23ff">
+            <p class="text-white mb-0 text-uppercase">S'inscrire
+              <span><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-right" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+            </svg></span>
+            </p>
           </div>
         </form>
       </div>
