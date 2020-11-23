f532a0a550aee56832910265ea3283c794e0ea1e
aouman <k2.aouman@gmail.com>
2020-11-23 11:39:47 +0000

    ajout d'une nouvelle section sous la section nos artistes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -109,78 +109,19 @@
           </div>
 
         </div>
-        <div class="row py-4">
-          <div class="col-md-2">
-            <div class="p-3 box-shadow rounded ">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
+      </section>
+      <section class="container-fluid">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="p-4">
+              <img src="/assets/images/abort.jpg" alt="" class="img-fluid">
             </div>
           </div>
-          <div class="col-md-2">
-            <div class="p-3 box-shadow rounded ">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
+          <div class="col-md-6">
+            <div class="p-4">
+              <p class="text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
             </div>
           </div>
-
-          <div class="col-md-2 ">
-            <div class="p-3 box-shadow rounded">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-2">
-            <div class="p-3 box-shadow rounded ">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-2">
-            <div class="p-3 box-shadow rounded ">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-md-2">
-            <div class="p-3 box-shadow rounded ">
-              <div class="">
-                <img src="https://process.filestackapi.com/AtM7HNKzQZ6u2HxwJF1Jiz/compress/quality=value:90/0tTy4z3lTbCkw18ehjQ8" alt="" class="img-fluid">
-              </div>
-              <div class="pt-2">
-                <h6 class="text-white font-weight-bold">Arafat DJ</h6>
-                <p class="text-white">Nouvel album</p>
-              </div>
-            </div>
-          </div>
-
         </div>
       </section>
     </div>
