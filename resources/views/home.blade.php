fa3455721e916215b3ee50b5e8a04b2863b793ba
aouman <k2.aouman@gmail.com>
2020-12-01 08:34:13 +0000

    Correction des text d'acueil

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,11 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-12 col-md-7 d-flex flex-column cta px-md-3 px-0 py-5">
-              <div class=" mb-md-5 ">
-                <p class="font-weight-bold display-4"> <span class="font-weight-lighter">Bienvenu sur  </span>Libanga</p>
-                <h4>Le Name-dropping à portée de clic </h4>
+              <div class=" mb-md-5 text-center">
+                <p class="font-weight-bold display-4"> <span class="font-weight-lighter">Bienvenue sur  </span>Libanga</p>
+                <h4>"Le Name-dropping à portée de clic"</h4>
               </div>
-              <div class="text-left mt-md-5 my-4">
+              <div class="text-left mt-md-5 my-4 ml-md-5">
                 <a href="#" class="text-decoration-none box-hover h-auto rounded-pill py-3 px-5 mt-3 mb-5 text-white btn-h btn-shadow" data-toggle="modal" data-target="#modalLogin">S'inscrire</a>
               </div>
             </div>
@@ -121,12 +121,14 @@
             </div>
           </div>
           <div class="col-md-6">
-            <div class="text-white">
-            <p class="py-md-5">Vous avez toujours entendu des noms de personnes êtres immortalisés dans des chansons de grands artistes, vous rêvez que le vôtre aussi soit cité ? Vous êtes au bon endroit.
+            <div class="text-white py-md-5">
+            <p class="">
+              Vous avez toujours entendu des noms de personnes êtres immortalisés dans des chansons de grands artistes, vous rêvez que le vôtre aussi soit cité ? Vous êtes au bon endroit.
+            </p>
+            <p class="">
               Libanga est l’intermédiaire de confiance entre votre artiste préféré et vous.
               Libanga s’assure que l’artiste vous dédicace bien dans son album et garantie à l’artiste le paiement de la prestation.
             </p>
-
           </div>
           </div>
         </div>
@@ -164,11 +166,16 @@
                   Payez
                 </li>
               </ul>
-              <p class="px-4 py-3">Vous n’avez plus qu’à patienter, nous prenons la suite.
-                Votre demande est transmise à l’artiste et nous nous assurons que le nom est bien dédicacé dans la chanson.
-                Si le nom est dédicacé dans la chanson, Libanga verse votre paiement à l’artiste.
-                Si ce n’est pas le cas, vous êtes automatiquement remboursé.
-              </p>
+              <div class="px-4 py-3">
+                <p >Vous n’avez plus qu’à patienter, nous prenons la suite.
+                  Votre demande est transmise à l’artiste et nous nous assurons que le nom est bien dédicacé dans la chanson.
+                </p>
+                <p>
+                  Si le nom est dédicacé dans la chanson, Libanga verse votre paiement à l’artiste.
+                  Si ce n’est pas le cas, vous êtes automatiquement remboursé.
+                </p>
+              </div>
+
             </div>
           </div>
           <div class="col-md-6">
