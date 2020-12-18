5702127b1d03e8367baf393b1cdd5ea0d756270c
aouman <k2.aouman@gmail.com>
2020-12-18 12:16:03 +0000

    la liste des commandes dans admin. j'ai crée un dossier achat directement dans view puis j'ai mis un index.blade.php dedans

--- resources/views/layouts/menuAdmin.blade.php
+++ resources/views/layouts/menuAdmin.blade.php
@@ -110,5 +110,38 @@
         });
       });
     </script>
+
+    <script>
+
+        $(document).ready(function(){
+          $("#courseSearch").keyup(function() {
+
+            // Retrieve the input field text and reset the count to zero
+            var filter = $(this).val(),
+              count = 0;
+
+            // Loop through the comment list
+            $("#contentUsers .filter").each(function() {
+
+
+              // If the list item does not contain the text phrase fade it out
+              if ($(this).text().search(new RegExp(filter, "i")) < 0) {
+                $(this).hide();
+
+                // Show the list item if the phrase matches and increase the count by 1
+              } else {
+                $(this).show();
+                count++;
+              }
+
+            });
+
+          });
+
+        });
+
+
+    </script>
+
   </body>
 </html>
