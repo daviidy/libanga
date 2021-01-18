15bac8d2b1d7098e730e7812926ae0363280ab22
aouman <k2.aouman@gmail.com>
2021-01-18 13:24:12 +0000

    delete plugin phone

--- resources/views/includes/usersPopup/popupEditDefault.blade.php
+++ resources/views/includes/usersPopup/popupEditDefault.blade.php
@@ -28,7 +28,7 @@
             </div>
             <div class="form-group">
               <label for="exampleInputPassword1">Téléphone</label>
-              <input type="tel" name="telephone" class="form-control" id="telephone">
+              <input type="tel" name="telephone" class="form-control" placeholder="+225 8905050500">
               @if (auth()->user()->telephone)
                 <small class="small-text" style="margin-left: 75px;color:red">Numero de télephone : {{auth()->user()->telephone}}</small>
               @endif
