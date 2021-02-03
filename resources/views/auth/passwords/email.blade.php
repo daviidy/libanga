34548ab536941b604d610367608cee01459d4507
aouman <k2.aouman@gmail.com>
2021-02-03 12:54:38 +0000

    designe des pages de modification du mot de passe.

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,43 @@
-@extends('layouts.app')
+@extends('layouts.menu')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Réinitialiser le mot de passe') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('Adresse email') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Envoyer le lien de réinitialisation') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<div class="container py-md-2">
+  <div class="row py-5">
+    <div class="col-md-12 p-md-5">
+      @if (session('status'))
+          <div class="alert alert-success" role="alert">
+              {{ session('status') }}
+          </div>
+      @endif
+    <div class="card mb-3 mx-auto" style="max-width: 540px;">
+      <div class="row no-gutters">
+        <div class="col-md-4 text-center">
+          <i class="bi bi-envelope" style="font-size: 120px"></i>
         </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title font-weight-bold">Réinitialiser le mot de passe</h5>
+            <form method="POST" action="{{ route('password.email') }}">
+              @csrf
+            <div class="form-group">
+              <label for="email">E-mail</label>
+              <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" id="email">
+              @error('email')
+                  <span class="invalid-feedback" role="alert">
+                      <strong>{{ $message }}</strong>
+                  </span>
+              @enderror
+            </div>
+
+            <button type="submit" class="btn btn-primary">Envoyer le lien de réinitialisation</button>
+          </form>
+          </div>
+        </div>
+      </div>
     </div>
+    </div>
+  </div>
+
+</div>
 </div>
 @endsection
